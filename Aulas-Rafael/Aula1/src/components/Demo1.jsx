b919f43d665ef5b6a7823bf7138cf26b5547aef2
L7noxy <Gabrielcastanhel2@gmail.com>
2025-02-06 17:20:43 -0300

    Atualizando a aula do dia 06/02/2025

--- Aulas-Rafael/Aula1/src/components/Demo1.jsx
+++ Aulas-Rafael/Aula1/src/components/Demo1.jsx
@@ -1,9 +1,48 @@
 import './Demo1.css'
+import { useState } from 'react';
 
 function Demo1() {
-  return (
-    <div className="container-demo1">Demo1</div>
-  )
+
+    const [estado, setEstado] = useEstate = 0;
+
+    let cornometro = 0;
+
+    function incrementarEstado(){
+        estado++
+    }
+
+    function maiszinho() {
+        cornometro++
+        console.log(cornometro);
+    }
+
+    function alertarUsuario() {
+        alert('voçê foi alertado!')
+    }
+
+    return (
+        <div className="container-demo1">
+
+            <h2>
+                Demo
+            </h2>
+
+            <button onClick={alertarUsuario}>Clica em mim</button>
+            <button onClick={() => { alert("Executei uma arrow function") }}>Ex de arrow funtion</button>
+
+            <div>
+                cornometro: {cornometro}            
+                <button onClick={maiszinho}>+</button>
+            </div>
+            <div>
+                estado: {estado}
+                <button onClick={incrementarEstado}>+</button>
+            </div>
+
+
+
+        </div>
+    )
 }
 
 export default Demo1
